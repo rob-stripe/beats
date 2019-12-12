90eecbfda73267d06c3629b6b0d3d25e29ac9311
Noémi Ványi <sitbackandwait@gmail.com>
2019-12-12 15:28:01 +0100

    move tools

--- dev-tools/tools.go
+++ dev-tools/tools.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	_ "golang.org/x/tools/cmd/goimports"
 	_ "golang.org/x/tools/go/ast/astutil"
 	_ "golang.org/x/tools/go/buildutil"
 	_ "golang.org/x/tools/go/loader"
 	_ "golang.org/x/tools/imports"
-	_ "golang.org/x/tools/internal/fastwalk"
-	_ "golang.org/x/tools/internal/gopathwalk"
 )
